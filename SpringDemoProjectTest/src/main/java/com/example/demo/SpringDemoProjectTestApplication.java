e82503d57c01b68dc53f9815e1dfa789e92f2969
Vicky <Vicky@Vicky-PC>
2020-04-12 01:14:19 +0530

    mainfileChanges

--- SpringDemoProjectTest/src/main/java/com/example/demo/SpringDemoProjectTestApplication.java
+++ SpringDemoProjectTest/src/main/java/com/example/demo/SpringDemoProjectTestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringDemoProjectTestApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Virendra Gupta");
 		SpringApplication.run(SpringDemoProjectTestApplication.class, args);
 	}
 
